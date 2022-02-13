16f954149490e525835428d8701837f5adc8c064
PRASHANT SHIVAJI MASRAM <63892767+prashant-112@users.noreply.github.com>
2022-02-13 11:29:52 +0530

    Update D14Q4 Largest rectangle in Histogram.cpp

--- D14Q4 Largest rectangle in Histogram.cpp
+++ D14Q4 Largest rectangle in Histogram.cpp
@@ -35,3 +35,27 @@ public:
         return MaxArea;
     }
 };
+
+
+//one pass soln
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights){
+        int n = heights.size();
+        stack<int> st;
+        int maxA = 0;
+        for(int i=0;i<=n;i++){ //iterate till n so that area of remaining element in stack can be computed
+            while(!st.empty() && (i==n || heights[st.top()] >= heights[i])){
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = i - st.top() - 1; //(RS-LS-1)
+                maxA = max(maxA,height*width);
+            }
+            st.push(i);
+        }
+        return maxA;
+    }
+};
